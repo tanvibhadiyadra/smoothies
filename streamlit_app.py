f3685a895cbf90559a17d80e6f8b7a929490b0c3
tanvibhadiyadra <tanvibhadiyadra@gmail.com>
2026-01-22 17:16:15 +0530

    Integrate API for ingredient nutrition information
    Added functionality to fetch and display nutrition information for selected ingredients from an external API.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 # from snowflake.snowpark.context import get_active_session
+import requests
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -21,27 +22,26 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 
 ingredients_list = st.multiselect(
     'choose up to 5 ingredients:',
-    my_dataframe , max_selections = 5
+    my_dataframe , max_selec
+    tions = 5
 )
 if ingredients_list:
     
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-    # st.write(ingredients_string)
-       
+        st.subheader(fruit_chosen + 'Nutrition Information')        
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
+        sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
+
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
                 values ('""" + ingredients_string + """','""" + name_on_order + """')"""
-    # st.write(my_insert_stmt)
-    # st.stop()
-    
+ 
     time_to_insert = st.button('Submit Order')
     
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
 
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-# st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
+
+
