1846fd2a95e0bef88db78d94874255832afb5bbd
tanvibhadiyadra <tanvibhadiyadra@gmail.com>
2026-01-22 15:54:07 +0530

    Comment out dataframe display and SQL insert statement
    Commented out code for displaying dataframe and SQL statement.

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,13 +10,13 @@ st.write(
   """
 )
 
-
 name_on_order = st.text_input("Name on Smoothie:")
 st.write("The Name on your Smoothie will be", name_on_order)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+# st.dataframe(data=my_dataframe, use_container_width=True)
 
 ingredients_list = st.multiselect(
     'choose up to 5 ingredients:',
@@ -27,15 +27,15 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-    
+    # st.write(ingredients_string)
        
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
                 values ('""" + ingredients_string + """','""" + name_on_order + """')"""
-    
+    # st.write(my_insert_stmt)
+    # st.stop()
     
     time_to_insert = st.button('Submit Order')
     
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
-    
